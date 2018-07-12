5e6714655bc474e9ded7044720cfd431cb4c4f93
genius-rabbit <U201613566@hust.edu.cn>
2018-07-12 11:00:48 +0800

    fix bug of factorial

--- firstWeek/factorial.sh
+++ firstWeek/factorial.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 # 计算阶乘
+# 由于$?无法返回大数字,使用全局变量存储返回的值
+
+let result=0
+let localreturn=0
+
+funFactorial(){
+    let num1=1
+    if [ $1 -gt $num1 ]; then
+        let numSub=$1-1
+        funFactorial $numSub
+        let result=$localreturn*$1
+        let localreturn=$result
+    else
+        let result=1
+        let localreturn=1;
+    fi
+}
 if [ -n "$1" ]; then
-    sum=1
-    i=$1
-    # 循环计算
-    while((i>0))
-    do
-        let sum=sum*i
-        let i=i-1
-    done
-    echo result:$sum
+    funFactorial $1
+    echo result:$result
 else
     echo "usage: factorial.sh [n]"
     echo "calculates a number's factorial"
