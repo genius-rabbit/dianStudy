f58b7331775e4c83d1e685f3764124919f92d044
genius-rabbit <U201613566@hust.edu.cn>
2018-07-13 17:37:53 +0800

    fix bug of file size get

--- firstWeek/file_size_get.sh
+++ firstWeek/file_size_get.sh
@@ -7,15 +7,17 @@
 if [ -n "$1" -o -n "$3" ];then
     # 有两个参数
     if [ -n "$1" -a -n "$3" ];then
-        du -ah $4 | sort -nr | head -$2
+        du -ak $4 | sort -nr | head -$2 > test.txt
     # 只有参数-n
     elif [ "-n" = "$1" ];then
-        du -ah | sort -nr | head -$2
+        du -ak | sort -nr | head -$2 > test.txt
     # 只有参数-d
     else
-        du -ah $2 | sort -nr
+        du -ak $2 | sort -nr > test.txt
     fi
 # 不存在参数
 else
-    du -ah | sort -nr
+    du -ah | sort -nr > test.txt
 fi
+cat -n test.txt
+rm test.txt
