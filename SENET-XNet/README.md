09d4bba92e0b8c3277b5f91a492ed298f1d6b734
genius-rabbit <U201613566@hust.edu.cn>
2018-08-16 11:09:25 +0800

    add photo

--- SENET-XNet/README.md
+++ SENET-XNet/README.md
@@ -4,5 +4,17 @@
 >>> python -m visdom.server
 ```
 - 可视化查看地址:http://localhost:8097/ or http://serverIP:8097/
-- visdom可视化截图
-![](./image/res_senet.png)
+- log图
+
+- 18层网络
+    - batchSize = 64 最终结果
+![](./image/SENet-batch64-last-18.png)
+    - batchSize = 64 最大结果
+![](./image/SENet-batch64-max-18.png)
+
+    - batchSize = 128 最终结果
+![](./image/SENet-batch128-last-18.png)
+
+- 34层网络
+    - batchSize = 64 最终结果
+    
--- SENET-XNet/SENET-ResNet-cuda.py
+++ SENET-XNet/SENET-ResNet-cuda.py
@@ -1,27 +1,23 @@
 import torch
 import math
 import torchvision
+from torch.autograd import Variable
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.utils.data.dataloader as Data
+import tensorflow as tf
 
-print('load data...')
+useTensorboard = False
+
+logdir = '/output/'
+testStep = 500
+netSize = [2, 2, 2, 2]
+epochSize = 100
+batch_size = 128
+root = './cifar'
 # root = '/home/geniusrabbit/PycharmProjects/TestPyTF/cifar100'
-root = '/input/cifar'
-train_data = torchvision.datasets.CIFAR100(
-    root=root, train=True, transform=transforms.ToTensor()
-)
-
-test_data = torchvision.datasets.CIFAR100(
-    root=root, train=False, transform=transforms.ToTensor()
-)
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-train_loader = Data.DataLoader(dataset=train_data, batch_size=128, shuffle=True, num_workers=2)
-test_loader = Data.DataLoader(dataset=test_data, batch_size=100, shuffle=False, num_workers=2)
-print('load data success...')
 
 
 class BasicBlock(nn.Module):
@@ -30,49 +26,6 @@ class BasicBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1):
         super(BasicBlock, self).__init__()
 
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(True)
-
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or inplanes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(inplanes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
-
-        self.fc1 = nn.Conv2d(planes, planes // 16, kernel_size=1)
-        self.fc2 = nn.Conv2d(planes // 16, planes, kernel_size=1)
-
-    def forward(self, x):
-        res = x
-
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-
-        x = self.conv2(x)
-        x = self.bn2(x)
-
-        w = F.avg_pool2d(x, x.size(2))
-        w = F.relu(self.fc1(w))
-        w = F.sigmoid(self.fc2(w))
-        x = x * w
-
-        x += self.shortcut(res)
-        x = self.relu(x)
-
-        return x
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(True)
@@ -129,14 +82,6 @@ class ResNet(nn.Module):
         self.avpool = nn.AvgPool2d(4)
         self.fc = nn.Linear(512, num_classes)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
     def _makelayer(self, block, planes, blocks, stride):
         strides = [stride]+[1]*(blocks-1)
         layers = []
@@ -165,60 +110,102 @@ class ResNet(nn.Module):
         return x
 
 
-net = ResNet(BasicBlock, [3, 4, 6, 3])
-if torch.cuda.is_available():
-    print('use gpu')
-    net = net.cuda()
-optimizer = optim.SGD(net.parameters(), lr=0.001)
-loss_func = nn.CrossEntropyLoss()
+def ResNet18():
+    return ResNet(BasicBlock, netSize)
 
-trainSize = 12500
-testSize = 2500
-print('testSize:%d, trainSize:%d' % (testSize, trainSize))
 
-print('Start to train...')
-for epoch in range(100):
+def train(model, data, target, loss_func, optimizer):
+    model.train()
+    optimizer.zero_grad()
+    output = model(data)
+    predictions = output.max(1, keepdim=True)[1]
+    correct = predictions.eq(target.view_as(predictions)).sum().item()
+    acc = correct / len(target)
+    loss = loss_func(output, target)
+    loss.backward()
+    optimizer.step()
+    return acc, loss
 
-    train_loss = 0
-    train_acc = 0
-    test_loss = 0
-    test_acc = 0
-    # train
-    for inputs, lables in train_loader:
-        optimizer.zero_grad()
-        if torch.cuda.is_available():
-            inputs = inputs.cuda()
-            lables = lables.cuda()
-        out = net(inputs)
 
-        loss = loss_func(out, lables)
-        train_loss += loss.item()
+def test(model, test_loader, loss_func, use_cuda):
+    model.eval()
+    acc_all = 0
+    loss_all = 0
+    step = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            step += 1
+            if use_cuda:
+                data = data.cuda()
+                target = target.cuda()
+            output = model(data)
+            predictions = output.max(1, keepdim=True)[1]
+            correct = predictions.eq(target.view_as(predictions)).sum().item()
+            acc = correct / len(target)
+            loss = loss_func(output, target)
+            acc_all += acc
+            loss_all += loss
+    return acc_all / step, loss_all / step
 
-        pred = out.max(1, keepdim=True)[1]
-        train_acc += pred.eq(lables.view_as(pred)).sum().item() / len(lables)
 
-        loss.backward()
-        optimizer.step()
+def main():
+    # tensorboard
+    if useTensorboard:
+        # use tensorboard
 
-    # test
-    for inputs, lables in test_loader:
-        if torch.cuda.is_available():
-            inputs = inputs.cuda()
-            lables = lables.cuda()
-        out = net(inputs)
+    use_cuda = torch.cuda.is_available()
+    train_loader = Data.DataLoader(
+        torchvision.datasets.CIFAR100(root=root, train=True, download=True, transform=
+        transforms.Compose([
+            transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+            transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])),
+        batch_size=batch_size,
+        shuffle=True
+    )
 
-        loss = loss_func(out, lables)
-        test_loss += loss.item()
+    test_loader = Data.DataLoader(
+        torchvision.datasets.CIFAR100(root=root, train=False,transform=
+        transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])),
+        batch_size=batch_size
+    )
 
-        pred = out.max(1, keepdim=True)[1]
-        test_acc += pred.eq(lables.view_as(pred)).sum().item() / len(lables)
+    # define network
+    model = ResNet18()
+    if use_cuda:
+        model = model.cuda()
+    print(model)
+    # define loss function
+    ce_loss = torch.nn.CrossEntropyLoss()
 
-    print('[%d] [Train] train_loss:%.6f train_acc:%.6f   [Test] test_loss:%.6f test_acc:%.6f' % (epoch + 1,
-                                                              train_loss / (trainSize),
-                                                              train_acc / (trainSize),
-                                                              test_loss / (testSize),
-                                                              test_acc / (testSize)))
+    # define optimizer
+    optimizer = optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, weight_decay=5e-4)
+    # writer = SummaryWriter()
+    # start train
+    train_step = 0
+    for _ in range(epochSize):
+        for data, target in train_loader:
+            train_step += 1
+            if use_cuda:
+                data = data.cuda()
+                target = target.cuda()
+            acc, loss = train(model, data, target, ce_loss, optimizer)
+            if train_step % 100 == 0:
+                print('[Train]: Step: {}, Loss: {:.6f}, Accuracy: {:.6f}'.format(train_step, loss, acc))
+                #writer.add_scalars('data/group', {'train_loss':loss, 'train_acc':acc}, train_step/100)
+            if train_step % testStep == 0:
+                acc, loss = test(model, test_loader, ce_loss, use_cuda)
+                print('[Test ]set: Step: {}, Loss: {:.6f}, Accuracy: {:.6f}\n'.format(train_step, loss, acc))
+                #writer.add_scalars('data/group', {'test_loss':loss, 'test_acc':acc}, train_step/100)
 
+    #writer.export_scalars_to_json("./all_scalars.json")
+    #writer.close()
+
+if __name__ == '__main__':
+    main()
 
 
 
