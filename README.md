5280133a6a251e981e08113a122b87862c4d9e18
genius-rabbit <U201613566@hust.edu.cn>
2018-08-18 09:17:37 +0800

    fix readme & senetResnet

--- README.md
+++ README.md
@@ -2,3 +2,13 @@
 
 ## firstWeek
 [Tree Scripts](https://github.com/genius-rabbit/dianStudy/tree/master/firstWeek)
+
+## neuralNetwork
+- AlexNet
+- GoogleNet
+- ResNet
+- VGG
+- leNet
+
+## SENET-XNet
+- SENET-ResNet
--- SENET-XNet/SENET-ResNet.py
+++ SENET-XNet/SENET-ResNet.py
@@ -1,27 +1,21 @@
 import torch
 import math
-import visdom
 import torchvision
-import numpy as np
+from torch.autograd import Variable
 import torch.optim as optim
 import torch.nn.functional as F
 import torchvision.transforms as transforms
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.utils.data.dataloader as Data
+from tensorboardX import SummaryWriter
+writer = SummaryWriter('/output/log')
 
-root = '/home/geniusrabbit/PycharmProjects/TestPyTF/cifar100'
-train_data = torchvision.datasets.CIFAR100(
-    root=root, train=True, transform=transforms.ToTensor()
-)
-
-test_data = torchvision.datasets.CIFAR100(
-    root=root, train=False, transform=transforms.ToTensor()
-)
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-train_loader = Data.DataLoader(dataset=train_data, batch_size=4, shuffle=True, num_workers=2)
-test_loader = Data.DataLoader(dataset=test_data, batch_size=4, shuffle=False, num_workers=2)
+testStep = 500
+netSize = [2, 2, 2, 2]
+epochSize = 100
+batch_size = 100
+root = './cifar'
+# root = '/home/geniusrabbit/PycharmProjects/TestPyTF/cifar100'
 
 
 class BasicBlock(nn.Module):
@@ -30,49 +24,6 @@ class BasicBlock(nn.Module):
     def __init__(self, inplanes, planes, stride=1):
         super(BasicBlock, self).__init__()
 
-        self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(True)
-
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or inplanes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(inplanes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
-
-        self.fc1 = nn.Conv2d(planes, planes // 16, kernel_size=1)
-        self.fc2 = nn.Conv2d(planes // 16, planes, kernel_size=1)
-
-    def forward(self, x):
-        res = x
-
-        x = self.conv1(x)
-        x = self.bn1(x)
-        x = self.relu(x)
-
-        x = self.conv2(x)
-        x = self.bn2(x)
-
-        w = F.avg_pool2d(x, x.size(2))
-        w = F.relu(self.fc1(w))
-        w = F.sigmoid(self.fc2(w))
-        x = x * w
-
-        x += self.shortcut(res)
-        x = self.relu(x)
-
-        return x
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, inplanes, planes, stride=1, downsample=None):
-        super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
         self.relu = nn.ReLU(True)
@@ -129,14 +80,6 @@ class ResNet(nn.Module):
         self.avpool = nn.AvgPool2d(4)
         self.fc = nn.Linear(512, num_classes)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
-
     def _makelayer(self, block, planes, blocks, stride):
         strides = [stride]+[1]*(blocks-1)
         layers = []
@@ -165,52 +108,99 @@ class ResNet(nn.Module):
         return x
 
 
-net = ResNet(BasicBlock, [3, 4, 6, 3])
-if torch.cuda.is_available():
-    net = net.cuda()
-print(net)
-# net = torch.load('ResNet.pkl')
+def ResNet18():
+    return ResNet(BasicBlock, netSize)
 
-optimizer = optim.SGD(net.parameters(), lr=0.01)
-loss_func = nn.CrossEntropyLoss()
 
-vis = visdom.Visdom(env='res_model')
+def train(model, data, target, loss_func, optimizer):
+    model.train()
+    optimizer.zero_grad()
+    output = model(data)
+    predictions = output.max(1, keepdim=True)[1]
+    correct = predictions.eq(target.view_as(predictions)).sum().item()
+    acc = correct / len(target)
+    loss = loss_func(output, target)
+    loss.backward()
+    optimizer.step()
+    return acc, loss
 
-for epoch in range(100):
 
-    train_loss = 0
-    train_acc = 0
-    test_acc = 0
+def test(model, test_loader, loss_func, use_cuda):
+    model.eval()
+    acc_all = 0
+    loss_all = 0
+    step = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            step += 1
+            if use_cuda:
+                data = data.cuda()
+                target = target.cuda()
+            output = model(data)
+            predictions = output.max(1, keepdim=True)[1]
+            correct = predictions.eq(target.view_as(predictions)).sum().item()
+            acc = correct / len(target)
+            loss = loss_func(output, target)
+            acc_all += acc
+            loss_all += loss
+    return acc_all / step, loss_all / step
 
-    for i, data in enumerate(train_loader, 0):
 
-        inputs, lables = data
-        inputs, lables = Variable(inputs), Variable(lables)
+def main():
+    use_cuda = torch.cuda.is_available()
+    train_loader = Data.DataLoader(
+        torchvision.datasets.CIFAR100(root=root, train=True, download=True, transform=
+        transforms.Compose([
+            transforms.RandomCrop(32, padding=4),  # 先四周填充0，在吧图像随机裁剪成32*32
+            transforms.RandomHorizontalFlip(),  # 图像一半的概率翻转，一半的概率不翻转
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])),
+        batch_size=batch_size,
+        shuffle=True
+    )
 
-        optimizer.zero_grad()
+    test_loader = Data.DataLoader(
+        torchvision.datasets.CIFAR100(root=root, train=False,transform=
+        transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))])),
+        batch_size=batch_size
+    )
 
-        out = net(inputs)
-        loss = loss_func(out, lables)
+    # define network
+    model = ResNet18()
+    if use_cuda:
+        model = model.cuda()
+    print(model)
+    # define loss function
+    ce_loss = torch.nn.CrossEntropyLoss()
 
-        train_loss += loss.item()
+    # define optimizer
+    optimizer = optim.SGD(model.parameters(), lr=1e-2, momentum=0.9, weight_decay=5e-4)
+    # writer = SummaryWriter()
+    # start train
+    train_step = 0
+    for _ in range(epochSize):
+        for data, target in train_loader:
+            train_step += 1
+            if use_cuda:
+                data = data.cuda()
+                target = target.cuda()
+            acc, loss = train(model, data, target, ce_loss, optimizer)
+            if train_step % 100 == 0:
+                print('[Train]: Step: {}, Loss: {:.6f}, Accuracy: {:.6f}'.format(train_step, loss, acc))
+                writer.add_scalars('data/group', {'train_loss':loss, 'train_acc':acc}, train_step/100)
+            if train_step % testStep == 0:
+                acc, loss = test(model, test_loader, ce_loss, use_cuda)
+                print('[Test ]set: Step: {}, Loss: {:.6f}, Accuracy: {:.6f}\n'.format(train_step, loss, acc))
+                writer.add_scalars('data/group', {'test_loss':loss, 'test_acc':acc}, train_step/100)
 
-        pred = torch.max(out, 1)[1]
-        train_acc += (pred == lables).sum().item()
+    writer.export_scalars_to_json("/output/all_scalars.json")
+    writer.close()
 
-        loss.backward()
-        optimizer.step()
-        if i % 100 == 99:
-            print(epoch+1, i)
+if __name__ == '__main__':
+    main()
 
-    for it, datat in enumerate(test_loader, 0):
-        inputs, lables = datat
-        inputs, lables = Variable(inputs), Variable(lables)
-        out = net(inputs)
-        pred = torch.max(out, 1)[1]
-        test_acc += (pred == lables).sum().item()
 
-    vis.line(Y=np.column_stack(np.array([train_loss / train_loader.__sizeof__(), train_acc / train_loader.__sizeof__(),
-                                         test_acc / test_loader.__sizeof__()])),
-             X=np.column_stack(np.array([epoch + 1, epoch + 1, epoch + 1])),
-             win='main',
-             update='append')
+
+
